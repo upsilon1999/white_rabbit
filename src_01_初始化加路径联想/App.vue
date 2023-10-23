1222bb649d857fc19438f1743bcba93a1fa5621a
upsilon1999 <Upsilon1999@outlook.com>
2023-10-23 22:37:33 +0800

    elementPlus自动按需引入

--- src_01_初始化加路径联想/App.vue
+++ src_01_初始化加路径联想/App.vue
@@ -3,7 +3,6 @@ import Hello from "@/components/Hello.vue"
 </script>
 
 <template>
-  <el-button type="primary">Primary</el-button>
   <RouterView />
 </template>
 
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//elementPlus按需引入，下面三项
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    //elementPlus按需引入配置，下面两项
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
