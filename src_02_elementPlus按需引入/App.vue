c8adb52a8cc5ca78c7d6ee16234c480d7ac29dc2
upsilon1999 <Upsilon1999@outlook.com>
2023-10-23 22:42:16 +0800

    elementsPlus主题的定制

---
+++ src_02_elementPlus按需引入/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Hello from "@/components/Hello.vue"
+</script>
+
+<template>
+  <el-button type="primary">Primary</el-button>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -14,15 +14,30 @@ export default defineConfig({
     vue(),
     //elementPlus按需引入配置，下面两项
     AutoImport({
-      resolvers: [ElementPlusResolver()],
+      // 自动引入修改主题色添加这一行，使用预处理样式，不添加将会导致使用ElMessage，ElNotification等组件时默认的主题色会覆盖自定义的主题色
+      resolvers: [
+        ElementPlusResolver({importStyle:"sass"})
+      ],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      // 自动引入修改主题色添加这一行，使用预处理样式，不添加将会导致使用ElMessage，ElNotification等组件时默认的主题色会覆盖自定义的主题色
+      resolvers: [
+        ElementPlusResolver({importStyle:"sass"})
+      ],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css:{
+		preprocessorOptions:{
+			scss:{
+				// 2.自动导入定制化样式文件进行样式覆盖
+                //此处的值采用了模板字符串
+				additionalData:`@use "@/styles/element/index.scss" as *;`,
+			}
+		}
+	}
 })
