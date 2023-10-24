59e2e439357dd23455b0adb1d6a62ab37fffbb90
upsilon1999 <Upsilon1999@outlook.com>
2023-10-24 23:15:54 +0800

    layout-静态结构的搭建

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,9 @@
 </script>
 
 <template>
-	<div class="test">
-		test
-	</div>
 	<!-- 一级路由出口组件 -->
 	<RouterView></RouterView>
 </template>
 
 <style lang="scss" scoped>
-.test{
-	color:$priceColor;
-}
 </style>
