9fafdeccfac61ff25b3836c0bc2fb49cc048f3c4
upsilon1999 <Upsilon1999@outlook.com>
2023-11-14 22:15:33 +0800

    图片懒加载指令的实现

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-// 之前默认的样式不需要
-// import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -17,7 +14,36 @@ Object.keys(Elicons).forEach((key) => {
     app.component(key, Elicons[key]);
 });
 
+//引入vueUse 监听目标元素可变性的插件
+import { useIntersectionObserver } from '@vueuse/core'
+
 app.use(createPinia())
+
 app.use(router)
 
 app.mount('#app')
+
+
+// 定义全局指令
+// 第一个参数是指令名字
+// 第二个是具体的配置对象
+app.directive("img-lazy",{
+	mounted(el,binding){
+		//el 指令绑定的元素
+		//binding 指令对象
+		//binding.value 指令表达式后面的值
+		// console.log(el);
+		// console.log(binding);
+		useIntersectionObserver(
+      el,
+      ([{ isIntersecting }]) => {
+      // isIntersecting 布尔值，元素进入视口区域为true，反之false
+        console.log(isIntersecting)
+        if (isIntersecting) {
+                //进入视口区域
+                el.src = binding.value
+        }
+      },
+    )
+	}
+})
